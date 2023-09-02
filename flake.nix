5088272c6aaf3052422c9a39fe491bb0c13ae822
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-09-02 12:16:44 +0200

    wip: make roc working with flake
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,21 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    roc-lang.url = "https://github.com/roc-lang/roc/archive/main.tar.gz";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, roc-lang, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          craneLib = crane.lib.${system};
+          roc = crane.lib.${system}.downloadCargoPackageFromGit {
+            git = "https://github.com/roc-lang/roc.git";
+            rev = "bf937b2b38d338072ba1f440c493015e9b583751";
+          };
       in {
         packages = {
-          default = pkgs.just;
+          default = roc;
         };
 
         formatter = pkgs.nixpkgs-fmt;
 
         devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.just roc-lang ];
+          nativeBuildInputs = [ pkgs.just pkgs.git ];
           shellHook = ''
             alias make=just
           '';
